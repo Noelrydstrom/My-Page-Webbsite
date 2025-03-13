371e387c72514345ae12264b77f69fa54e7dbf52
Noelrydstrom <ironnoel33@gmail.com>
2025-03-13 15:05:29 +0100

    Add files via upload

---
+++ JsSkript.js
@@ -0,0 +1,68 @@
+alert("Är du säker på att du vill du öppna världens bästa Webbsida?");
+//Toggle Icon navbar//
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+//What happens on scroll: When a section comes into view, its corresponding navigation link is highlighted by adding the active class. When the user scrolls 
+//past it, the class is removed from that link.//
+//Scroll Section = //
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    //Sticky Navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //Remove Toggle icon and navbar when click on navbar link (scroll)//
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+    //Scroll Reveal//
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 
+'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+    //typed js//
+const typed = new Typed('.multiple-text',{
+    strings: ['Fullstack Developer', 'Frontend Developer', 'Backend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+//OPen cv
+function loadNewPage() {
+    window.location.href = "https://www.example.com";  
+  };
